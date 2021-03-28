556c3a71d048fb3f8539f77643f4d5faee75f1ff
Mohammed Irshad <irshad@ideabits.se>
2021-03-28 20:39:10 +0530

    store selection in neo4j databse

--- server.js
+++ server.js
@@ -5,8 +5,6 @@ const cors = require("cors");
 const app = express();
 app.use(cors());
 
-// npm install --save neo4j-driver
-// node example.js
 const neo4j = require("neo4j-driver");
 
 const port = 3200;
@@ -16,56 +14,49 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 router.post("/api/save", (request, response) => {
-  //code to perform particular action.
-  //To access POST variable use req.body()methods.
   console.log(request.body);
 
-  // const splitArray = {};
-  // request.body.forEach((item, key) => {
-  //   splitArray[key] = item;
-  // });
-  // console.log("splitArray", splitArray);
-  const params = {};
-  request.body.forEach((item, key) => {
-    // const query = `MERGE (user:User{name:'TheOneAndOnlyUser'})-[choice:HAS_CHOSEN_PHOTO]->(photo:Photo) SET photo = $itemKey RETURN photo, user`;
+  const driver = neo4j.driver(
+    "bolt://34.238.220.27:7687",
+    neo4j.auth.basic("neo4j", "vent-election-quiets"),
+    {
+      /* encrypted: 'ENCRYPTION_OFF' */
+    }
+  );
 
-    const query = `MATCH (user:User{name:"TheOneAndOnlyUser"}) MERGE (photo:Photo) SET photo = $item_${key} MERGE (user)-[choice:HAS_CHOSEN_PHOTO]->(photo)  RETURN photo, user`;
-    console.log("query: ", query);
+  // Delete existing selection before storing new one
+  const deleteQuery = `match (p:Photo) detach delete p return p`;
+  const session = driver.session({ database: "neo4j" });
+  const result = [];
+  return session
+    .run(deleteQuery)
+    .then(() => {
+      /// Now store the new selection
+      params = {};
+      request.body.forEach((item, key) => {
+        const itemKey = `item_${key}`;
+        params[itemKey] = item;
 
-    const itemKey = `item_${key}`;
-    params[itemKey] = item;
-    console.log("params: ", params);
+        const query = `MATCH (user:User{name:"TheOneAndOnlyUser"}) CREATE (photo:Photo) SET photo = $item_${key} MERGE (user)-[choice:HAS_CHOSEN_PHOTO]->(photo)  RETURN photo, user`;
 
-    const driver = neo4j.driver(
-      "bolt://34.238.220.27:7687",
-      neo4j.auth.basic("neo4j", "vent-election-quiets"),
-      {
-        /* encrypted: 'ENCRYPTION_OFF' */
-      }
-    );
+        const session = driver.session({ database: "neo4j" });
 
-    const session = driver.session({ database: "neo4j" });
-
-    return session
-      .run(query, params)
-      .then((result) => {
-        console.log("result: ", result);
-        console.log("params: ", params);
-        console.log("query: ", query);
-        result.records.forEach((record) => {
-          console.log("RECORD-COUNT", record.get("user"));
-        });
-        session.close();
-        driver.close();
-      })
-      .catch((error) => {
-        console.error("NEO4J -ERROR", error);
-      })
-      .finally(() => {
-        session.close();
-        response.end("yes");
+        return session
+          .run(query, params)
+          .then((result) => {
+            result.records.forEach((record) => {
+              console.log("RECORD-COUNT", record.get("photo"));
+              result.push(record.get("photo"));
+            });
+          })
+          .catch((error) => {
+            console.error("NEO4J -ERROR", error);
+          });
       });
-  });
+    })
+    .catch((error) => {
+      console.error("NEO4J -ERROR", error);
+    });
 });
 
 // add router in the Express app.
