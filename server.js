f37a59db5fd02c217463ce7d38cd0149e6a74014
Mohammed Irshad <irshad@ideabits.se>
2021-03-28 23:40:39 +0530

    perform some level of refactoring

--- server.js
+++ server.js
@@ -3,43 +3,58 @@
  * 1. /api/save to save the selected images that are sorted
  * 2. /api/get-images to retrieve the stored selection of images
  *
- * The data is stored in a remote Neo4j database using cypher queries
+ * NOTE : Database The data is stored in a remote Neo4j database using cypher queries
+ * This Sandbox database will expire in 10 days (5th of April 2021). if so please contact me (Mohamed Irshad)
+ * via email so I can spin up a new instance
  */
 const express = require("express");
 const bodyParser = require("body-parser");
-const router = express.Router();
 const cors = require("cors");
+const router = express.Router();
+const neo4j = require("neo4j-driver");
 const app = express();
 
 app.use(cors());
-const neo4j = require("neo4j-driver");
-const port = 3200;
-
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use("/", router);
 
 /**
+ * DEFINE APPLICATION CONSTANTS HERE
+ */
+const PORT = 3200;
+const SAVE_API = "/api/save";
+const RETRIEVE_API = "/api/get-images";
+const DELETE_API = "/api/delete-images";
+const DATABASE_HOST = "bolt://34.238.220.27:7687";
+const DATABASE_USER = "neo4j";
+const DATABASE_PASSWORD = "vent-election-quiets";
+
+const initializeDatabaseDriver = () => {
+  return neo4j.driver(
+    DATABASE_HOST,
+    neo4j.auth.basic(DATABASE_USER, DATABASE_PASSWORD),
+    {
+      /* encrypted: 'ENCRYPTION_OFF' */
+    }
+  );
+};
+
+/**
+ *
  * API endpoint to store selected images that are sorted
  * @param {array} array of sorted images
  * @returns {String} Ok if success
  * @returns {String} "Something went wrong!" if there was an generic error
  * @returns {String} "Something broke!" if there was a server error
+ *
  */
-router.post("/api/save", (request, response) => {
+router.post(SAVE_API, (request, response) => {
   console.log(request.body);
 
-  const driver = neo4j.driver(
-    "bolt://34.238.220.27:7687",
-    neo4j.auth.basic("neo4j", "vent-election-quiets"),
-    {
-      /* encrypted: 'ENCRYPTION_OFF' */
-    }
-  );
+  const driver = initializeDatabaseDriver();
 
-  /***
-   *  Neo4j Query --> Delete existing selection before storing new one
-   * */
+  // Neo4j Query --> Delete existing selection before storing new one
   const deleteQuery = `match (p:Photo) detach delete p return p`;
 
   const session = driver.session({ database: "neo4j" });
@@ -47,127 +62,126 @@ router.post("/api/save", (request, response) => {
     return session
       .run(deleteQuery)
       .then(() => {
-        /// Now store the new selection
+        // Now store the new selection in the database
         params = {};
-        const savedArray = [];
+        const dbResultArray = [];
         request.body.forEach((item, key) => {
           const itemKey = `item_${key}`;
           params[itemKey] = { ...item, key };
 
-          /***
-           *  Neo4j Query --> save existing selection one by one
-           * */
+          //  Neo4j Query --> save existing selection one by one
           const query = `MATCH (user:User{name:"TheOneAndOnlyUser"}) CREATE (photo:Photo) SET photo = $item_${key} MERGE (user)-[choice:HAS_CHOSEN_PHOTO]->(photo)  RETURN photo, user`;
 
+          // declare a seperate session for the storing process
           const session = driver.session({ database: "neo4j" });
 
           return session
             .run(query, params)
             .then((result) => {
               result.records.forEach((record) => {
-                console.log("RECORD-COUNT", record.get("photo"));
-                console.log("------", record.get("photo").properties);
-                savedArray.push(record.get("photo").properties);
+                // add every recored saved to the dbResultArray array
+                dbResultArray.push(record.get("photo").properties);
               });
-              if (savedArray.length === request.body.length) {
+              if (dbResultArray.length === request.body.length) {
+                // send success after all records have been stored
                 return response.send("Ok");
               }
             })
             .catch((error) => {
-              console.error("NEO4J CREATE QUERY -ERROR", error);
+              console.error("Neo4j create query error", error);
+              response.status(500).send("Neo4j create query error!");
             });
         });
       })
       .catch((error) => {
-        console.error("NEO4J DELETE -ERROR", error);
+        console.error("Neo4j Delete error", error);
         response.status(500).send("Something broke!");
       });
   } catch (error) {
+    console.log("Server error: ", error);
     response.status(400).send("Something went wrong!");
   }
 });
 
 /**
+ *
  * API endpoint to retrieve selected images that are sorted
  * @returns {Array} array of selected images if success
  * @returns {String} "Something went wrong!" if there was an generic error
  * @returns {String} "Something broke!" if there was a server error
+ *
  */
-app.get("/api/get-images", (request, response) => {
-  const driver = neo4j.driver(
-    "bolt://34.238.220.27:7687",
-    neo4j.auth.basic("neo4j", "vent-election-quiets"),
-    {
-      /* encrypted: 'ENCRYPTION_OFF' */
-    }
-  );
+app.get(RETRIEVE_API, (request, response) => {
+  const driver = initializeDatabaseDriver();
 
-  /***
-   *  Neo4j Query --> match existing selection and return the photos
-   * */
+  // Neo4j Query --> match existing selection and return the photos
   const retrieveQuery = `match (user:User)-[r:HAS_CHOSEN_PHOTO]->(photo:Photo) return photo ORDER BY photo.key`;
-  const session = driver.session({ database: "neo4j" });
   try {
     const session = driver.session({ database: "neo4j" });
 
-    const savedArray = [];
+    const dbResultArray = [];
     return session
       .run(retrieveQuery)
       .then((result) => {
         result.records.forEach((record) => {
-          console.log("RECORD-COUNT", record.get("photo"));
-          console.log("------", record.get("photo").properties);
-          savedArray.push(record.get("photo").properties);
+          // add every recored saved to the dbResultArray array
+          dbResultArray.push(record.get("photo").properties);
         });
-        if (savedArray.length === 9) {
-          return response.send(savedArray);
+        if (dbResultArray.length === 9) {
+          // send success after all records have been retrieved
+          return response.send(dbResultArray);
         }
       })
       .catch((error) => {
-        console.error("NEO4J RETRIEVE QUERY -ERROR", error);
+        console.error("Neo4j retrieve query error", error);
+        response.status(500).send("Neo4j retrieve query error!");
       });
   } catch (error) {
+    console.log("Server error: ", error);
     response.status(400).send("Something went wrong!");
   }
 });
 
 /**
+ *
  * API endpoint to delete selected images for a fresh user
  * @returns {String} "Ok. deleted all" if success
  * @returns {String} "Something went wrong!" if there was an generic error
  * @returns {String} "Something broke!" if there was a server error
+ *
  */
-app.get("/api/delete-images", (request, response) => {
-  const driver = neo4j.driver(
-    "bolt://34.238.220.27:7687",
-    neo4j.auth.basic("neo4j", "vent-election-quiets"),
-    {
-      /* encrypted: 'ENCRYPTION_OFF' */
-    }
-  );
 
-  /***
-   *  Neo4j Query --> delete existing selection and return the photos
-   * */
+app.get(DELETE_API, (request, response) => {
+  const driver = initializeDatabaseDriver();
+
+  // Neo4j Query --> delete existing selection and return the photos
   const deleteQuery = `match (p:Photo) detach delete p return p `;
-  const session = driver.session({ database: "neo4j" });
   try {
     const session = driver.session({ database: "neo4j" });
 
-    const savedArray = [];
     return session
       .run(deleteQuery)
       .then(() => {
+        // return success if delte query succeeded
         return response.send("Ok. deleted all");
       })
       .catch((error) => {
-        console.error("NEO4J DELETE ALL QUERY -ERROR", error);
+        console.error("neo4j delete all query error ", error);
+        response.status(500).send("Neo4j retrieve query error!");
       });
   } catch (error) {
+    console.log("Server error: ", error);
     response.status(400).send("Something went wrong!");
   }
 });
 
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
+app.listen(PORT, () => {
+  console.log(`PastBook photo app listening at http://localhost:${PORT}`);
+  console.log(`Save images API      ----> http://localhost:${PORT}${SAVE_API}`);
+  console.log(
+    `Retrieve images API  ----> http://localhost:${PORT}${RETRIEVE_API}`
+  );
+  console.log(
+    `Delete Images API    ----> http://localhost:${PORT}${DELETE_API}`
+  );
 });
