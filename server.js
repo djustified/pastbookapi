33fb9e433e2c2797fae6113d9c7e48d5d8d2da62
Mohammed Irshad <irshad@ideabits.se>
2021-03-28 12:12:09 +0530

    make simple api response

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const bodyParser = require("body-parser");
 const router = express.Router();
 const cors = require("cors");
 const app = express();
+
 app.use(cors());
 
 const port = 3200;
@@ -10,6 +11,8 @@ const port = 3200;
 //Here we are configuring express to use body-parser as middle-ware.
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+// add router in the Express app.
+app.use("/", router);
 
 router.post("/api/save", (request, response) => {
   //code to perform particular action.
@@ -18,8 +21,11 @@ router.post("/api/save", (request, response) => {
   response.end("yes");
 });
 
-// add router in the Express app.
-app.use("/", router);
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/index.html");
+  // Note: __dirname is the current directory you're in. Try logging it and see what you get!
+  // Mine was '/Users/zellwk/Projects/demo-repos/crud-express-mongo' for this app.
+});
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
